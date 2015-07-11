34ec39a36bfe9f0d9a96b4a4fb002ffb50ed4352
Anatoliy Kukul <anatolkukula@gmail.com>
2015-07-11 09:33:52 +0200

    Improve tmux copy-pasting experience
    Change tmux bind to ctrl u

--- tmux.conf.sh
+++ tmux.conf.sh
@@ -1,14 +1,45 @@
 unbind C-b
+set -g prefix C-u
+bind C-u send-prefix
 
-set -g prefix C-a
+set -g default-terminal "xterm-256color"
 
 set -g base-index 1
 set -g pane-base-index 1
-set -g default-terminal "xterm-256color"
 set -g set-titles on
 set -g set-titles-string "#S:#I:#P #W #T"
 set -g status-utf8 on
 set -g status-right "#[fg=white]%H:%M#[default]"
+
+setw -g automatic-rename
+
+# Key bindings
+bind r source-file ~/.tmux.conf \; display "Reloaded config."
+
+bind -r h select-pane -L
+bind -r j select-pane -D
+bind -r k select-pane -U
+bind -r l select-pane -R
+
+# Sessions
+new
+
+# Copy-paste integration
+set-option -g default-command "reattach-to-user-namespace -l bash"
+
+# Use vim keybindings in copy mode
+setw -g mode-keys vi
+
+# Setup 'v' to begin selection as in Vim
+bind-key -t vi-copy v begin-selection
+bind-key -t vi-copy y copy-pipe "reattach-to-user-namespace pbcopy"
+
+# Update default binding of `Enter` to also use copy-pipe
+unbind -t vi-copy Enter
+bind-key -t vi-copy Enter copy-pipe "reattach-to-user-namespace pbcopy"
+
+# Bind ']' to use pbpaste
+bind ] run "reattach-to-user-namespace pbpaste | tmux load-buffer - && tmux paste-buffer"
 
 #### COLOUR (Solarized 256)
 
@@ -41,17 +72,3 @@ set-option -g display-panes-colour colour166 #orange
 
 # clock
 set-window-option -g clock-mode-colour colour64 #green
-setw -g automatic-rename
-
-# Key bindings
-
-bind r source-file ~/.tmux.conf \; display "Reloaded config."
-
-bind -r h select-pane -L
-bind -r j select-pane -D
-bind -r k select-pane -U
-bind -r l select-pane -R
-
-# Sessions
-
-new
