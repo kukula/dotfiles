e0112b5d1d4bca594740055c8df130cf0799d894
Anatoliy Kukul <anatolkukula@gmail.com>
2015-10-09 16:34:31 +0200

    Update tmux config

--- tmux.conf.sh
+++ tmux.conf.sh
@@ -1,4 +1,9 @@
-set -g default-terminal "xterm-256color"
+set-option -g default-command "reattach-to-user-namespace -l bash"
+
+set -g history-limit 10000
+
+set -g base-index 1
+set -g pane-base-index 1
 
 set -g base-index 1
 set -g pane-base-index 1
@@ -7,18 +12,26 @@ set -g set-titles-string "#S:#I:#P #W #T"
 set -g status-utf8 on
 set -g status-right "#[fg=white]%H:%M#[default]"
 
-setw -g automatic-rename
+# Colors
+set -g default-terminal "screen-256color"
+set -g status-bg colour235
+set -g status-fg white
+
+set -g status-left ""
+set -g status-right "%R"
+
+set-window-option -g window-status-current-fg black
+set-window-option -g window-status-current-bg white
+
+set -g pane-border-fg colour235
+set -g pane-border-bg black
+set -g pane-active-border-fg white
 
 # Key bindings
-bind r source-file ~/.tmux.conf \; display "Reloaded config."
-
 bind -r h select-pane -L
 bind -r j select-pane -D
 bind -r k select-pane -U
 bind -r l select-pane -R
-
-# Sessions
-new
 
 # Copy-paste integration
 set-option -g default-command "reattach-to-user-namespace -l bash"
@@ -36,35 +49,3 @@ bind-key -t vi-copy Enter copy-pipe "reattach-to-user-namespace pbcopy"
 
 # Bind ']' to use pbpaste
 bind ] run "reattach-to-user-namespace pbpaste | tmux load-buffer - && tmux paste-buffer"
-
-#### COLOUR (Solarized 256)
-
-# default statusbar colors
-set-option -g status-bg colour235 #base02
-set-option -g status-fg colour136 #yellow
-set-option -g status-attr default
-
-# default window title colors
-set-window-option -g window-status-fg colour244 #base0
-set-window-option -g window-status-bg default
-#set-window-option -g window-status-attr dim
-
-# active window title colors
-set-window-option -g window-status-current-fg colour166 #orange
-set-window-option -g window-status-current-bg default
-#set-window-option -g window-status-current-attr bright
-
-# pane border
-set-option -g pane-border-fg colour235 #base02
-set-option -g pane-active-border-fg colour240 #base01
-
-# message text
-set-option -g message-bg colour235 #base02
-set-option -g message-fg colour166 #orange
-
-# pane number display
-set-option -g display-panes-active-colour colour33 #blue
-set-option -g display-panes-colour colour166 #orange
-
-# clock
-set-window-option -g clock-mode-colour colour64 #green
