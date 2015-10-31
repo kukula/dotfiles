aea88d94f5b327f61d319bf9b0b6cf353297dab0
Anatoliy Kukul <anatolkukula@gmail.com>
2015-10-31 12:43:12 +0100

    Update tmux conf

--- tmux.conf.sh
+++ tmux.conf.sh
@@ -1,51 +1,60 @@
-set-option -g default-command "reattach-to-user-namespace -l bash"
+# play nice with OSX copy/paste tools
+set -g default-command "reattach-to-user-namespace /bin/bash --login"
+bind y run-shell "tmux save-buffer - | reattach-to-user-namespace pbcopy"
 
-set -g history-limit 10000
+# spliting
+bind | split-window -h # C-b | to create horizontal pane
+bind - split-window -v # C-b - to create vertical pane
 
+# moving between panes
+bind h select-pane -L
+bind j select-pane -D
+bind k select-pane -U
+bind l select-pane -R
+
+# resize panes - C-b <arrow key>
+unbind Up
+bind Up resize-pane -U 4
+unbind Down
+bind Down resize-pane -D 4
+unbind Left
+bind Left resize-pane -L 10
+unbind Right
+bind Right resize-pane -R 10
+
+set -g default-terminal screen-256color
+setw -g mode-keys vi # enable vi keys
+
+# start windows and panes at 1, not 0
 set -g base-index 1
 set -g pane-base-index 1
 
-set -g base-index 1
-set -g pane-base-index 1
-set -g set-titles on
-set -g set-titles-string "#S:#I:#P #W #T"
-set -g status-utf8 on
-set -g status-right "#[fg=white]%H:%M#[default]"
-
-# Colors
-set -g default-terminal "screen-256color"
-set -g status-bg colour235
-set -g status-fg white
-
+#### COLOURS
+# minimal status bar with clock
 set -g status-left ""
-set -g status-right "%R"
+set -g status-right "#[fg=colour244]%R"
 
-set-window-option -g window-status-current-fg black
-set-window-option -g window-status-current-bg white
+# default statusbar colors
+set-option -g status-bg black
+set-option -g status-fg white
+set-option -g status-attr default
 
-set -g pane-border-fg colour235
-set -g pane-border-bg black
-set -g pane-active-border-fg white
+# default window title colors
+set-window-option -g window-status-fg white
+set-window-option -g window-status-bg black
 
-# Key bindings
-bind -r h select-pane -L
-bind -r j select-pane -D
-bind -r k select-pane -U
-bind -r l select-pane -R
+# active window title colors
+set-window-option -g window-status-current-fg white
+set-window-option -g window-status-current-bg colour240
 
-# Copy-paste integration
-set-option -g default-command "reattach-to-user-namespace -l bash"
+# title format
+set -g window-status-format " #[fg=colour240]#I #[fg=colour244]#W "
+set -g window-status-current-format " #[fg=colour244]#I #[fg=white]#W "
 
-# Use vim keybindings in copy mode
-setw -g mode-keys vi
+# pane border
+set-option -g pane-border-fg colour240
+set-option -g pane-active-border-fg colour3 #orange
 
-# Setup 'v' to begin selection as in Vim
-bind-key -t vi-copy v begin-selection
-bind-key -t vi-copy y copy-pipe "reattach-to-user-namespace pbcopy"
-
-# Update default binding of `Enter` to also use copy-pipe
-unbind -t vi-copy Enter
-bind-key -t vi-copy Enter copy-pipe "reattach-to-user-namespace pbcopy"
-
-# Bind ']' to use pbpaste
-bind ] run "reattach-to-user-namespace pbpaste | tmux load-buffer - && tmux paste-buffer"
+# message text
+set-option -g message-bg colour240
+set-option -g message-fg colour3 #orange
