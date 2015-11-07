c2dfec3552d086a6537fce13a8dc4245f52df3b7
Anatoliy Kukul <anatolkukula@gmail.com>
2015-11-07 10:09:54 +0100

    Clean up bash profile and enable colors

--- bash/profile.sh
+++ bash/profile.sh
@@ -1,18 +1,25 @@
 # In case of nano
 export EDITOR=vim
 
-# Base16 Shell collor switching
+# Enable colors
+export CLICOLOR=1
+
+# Base16 Shell color switching
 if [ -n "$PS1" ]; then # if statement guards adding these helpers for non-interative shells
   eval "$(~/dotfiles/base16-shell/profile_helper.sh)"
 fi
 
-# Promt 
+# Promt
 PROMPT_SHELL="$HOME/dotfiles/bash/prompt.sh"
 [[ -s $PROMPT_SHELL ]] && source $PROMPT_SHELL
 
-function ghost_mode {
-  export PS1="👻  "
-}
+# Ignore space and duplicates in history
+export HISTCONTROL=ignoreboth:erasedups
+
+# Colored grep
+alias grep='grep -v grep | grep --color=auto'
+alias fgrep='fgrep --color=auto'
+alias egrep='egrep --color=auto'
 
 export PATH="/usr/local/bin:$PATH"
 
@@ -21,18 +28,8 @@ if [ -f ~/dotfiles/bash/git-completion.bash ]; then
   . ~/dotfiles/bash/git-completion.bash
 fi
 
-# JAVA
-#export JAVA_HOME="$(/usr/libexec/java_home)"
-#export JAVA_OPTS="-Xmx3g -Xms1g"
-#export JRUBY_OPTS="-Xcompile.invokedynamic=false -J-XX:+TieredCompilation -J-XX:TieredStopAtLevel=1 -J-noverify -Xcompile.mode=OFF"
-
 # Postgress.app
 export PATH="/Applications/Postgres.app/Contents/Versions/9.4/bin:$PATH"
-
-# Go
-export GOROOT=/usr/local/go
-export GOPATH=$HOME/go
-export PATH=$GOROOT/bin:$GOPATH/bin:$PATH
 
 # NPM
 export PATH=$HOME/npm/bin:$PATH
@@ -45,9 +42,8 @@ export PATH="$HOME/.rvm/bin:$PATH" # Add RVM to PATH for scripting
 export PHANTOMJS_BIN=phantomjs
 
 # Aliases
-alias ll='ls -hal'
-alias v='vim'
-alias g='git'
+alias la='ls -hal'
+alias ll='ls -hAl'
 alias gst='git status'
 alias gc='git commit'
 alias gco='git checkout'
@@ -58,11 +54,14 @@ alias gl='git pull'
 alias gp='git push'
 alias gs='git show'
 alias gb='git branch'
+alias glo='git log'
 
-alias bi='bundle install'
-alias be='bundle exec'
+# Simple calc in cli
+function calc () {
+  { echo "$*" | bc -l; }
+}
 
-alias r='rails'
-
-alias pass='passenger start'
-alias torq='torquebox deploy && torquebox run'
+# Alias for 'ps aux | grep'
+function psgrep () {
+  { ps aux | grep "$*"; }
+}
