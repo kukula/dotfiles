3be8daa5032132417609eac9047ae661c3c46b8a
Tolik <anatolkukula@gmail.com>
2023-07-02 19:02:34 +1000

    Initial for Lazyvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,23 +1,2 @@
-local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
-
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    'git',
-    'clone',
-    '--filter=blob:none',
-    'https://github.com/folke/lazy.nvim.git',
-    '--branch=stable',
-    lazypath,
-  })
-end
-
-vim.opt.rtp:prepend(lazypath)
-
-vim.g.mapleader = ','
-vim.g.maplocalleader = ','
-
-require('lazy').setup(require('plugins'))
-require('general')
-
--- The line beneath this is called `modeline`. See `:help modeline`
--- vim: ts=2 sts=2 sw=2 et
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
